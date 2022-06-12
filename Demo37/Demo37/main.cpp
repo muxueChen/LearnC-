b25de1ef75c946dd317eafed8f13bbd75bf2215c
暮雪 <1391801900@qq.com>
2022-06-12 17:58:02 +0800

    整理Effective C++ 第28、36、37条款

--- Demo37/Demo37/main.cpp
+++ Demo37/Demo37/main.cpp
@@ -7,6 +7,28 @@
 
 #include <iostream>
 
+class Shape {
+public:
+    enum ShapeColor {Red, Green, Blue};
+    void draw(ShapeColor color = Red) {
+        doDraw(color);
+    }
+private:
+    virtual void doDraw(ShapeColor color) {// 真正实现
+        
+    }
+};
+
+class Rectangle:Shape {
+    
+public:
+    //...
+private:
+    void doDraw(ShapeColor color) override {// 不需要制定缺省参数
+        //。。。
+    }
+};
+
 int main(int argc, const char * argv[]) {
     // insert code here...
     std::cout << "Hello, World!\n";
